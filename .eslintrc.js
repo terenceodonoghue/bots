77e1307c9945dd21d4248630c9494818db1b8b71
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2022-05-09 20:13:20 +1000

    Add Nx to monorepo, update ESlint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,32 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
 };
