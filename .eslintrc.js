7c6e0f7fc43dcc6e4ca3161827620ab8bb94af91
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2022-06-13 16:16:38 +1000

    Update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,6 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  plugins: ['@typescript-eslint'],
+  extends: ['@terenceodonoghue/node'],
   rules: {
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
     'no-underscore-dangle': ['error', { allowAfterThis: true }],
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
   },
 };
